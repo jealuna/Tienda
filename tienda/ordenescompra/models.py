6cf1de6a123343c72636ef4bc848021fe0922671
Alejandro Luna <alejandro.luna@ciencias.unam.mx>
2020-02-13 11:42:31 -0600

    Se agrega modelo para ordenes de productos

--- tienda/ordenescompra/models.py
+++ tienda/ordenescompra/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from productos.models import producto
+from almacenes.models import almacen
 
 # Create your models here.
+class ProductoOrden(models.Model):
+    """Model definition for ProductoOrden."""
+    modelo = models.ForeignKey(producto, on_delete=models.CASCADE)
+    subinventario = models.ForeignKey(almacen, on_delete=models.CASCADE) 
+    imei = models.CharField(max_length=15)
+    
+
+    class Meta:
+        """Meta definition for ProductoOrden."""
+
+        verbose_name = 'ProductoOrden'
+        verbose_name_plural = 'ProductoOrdens'
+
+    def __str__(self):
+        """Unicode representation of ProductoOrden."""
+        pass
+
