51bbb4fe1b14b530803424659a22c980fef4b9f1
Jesus Luna <jlunar@ultrasist.com.mx>
2020-02-13 00:56:42 -0600

    Se crean pruebas unitarias para almacenes y productos

--- tienda/almacenes/tests.py
+++ tienda/almacenes/tests.py
@@ -1,3 +1,24 @@
+import json
+from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework.test import APITestCase, force_authenticate
+from almacenes.models import almacen
+from almacenes.serializers import AlmacenSerializer
 
-# Create your tests here.
+class AlmacenTestCase(APITestCase):
+    def setUp(self):
+        self.username = 'john_doe'
+        self.password = 'foobar'
+        self.user = User.objects.create(username=self.username, password=self.password)
+        self.client.force_authenticate(user=self.user)
+        almacen.objects.create(
+            subinventario='IDBCA00002', nombre='BCA-PLAZACENTELLAEDOMEX')
+        almacen.objects.create(
+            subinventario='IDBCA00003', nombre='BCA - TAXQUEÑA EDUCACION')
+
+    def test_get_lista(self):
+        response = self.client.get('/api/almacenes/', format='json')
+        almacenes = almacen.objects.all()
+        serializer = AlmacenSerializer(almacenes, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, 200)
--- tienda/productos/tests.py
+++ tienda/productos/tests.py
@@ -1,3 +1,24 @@
+import json
+from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework.test import APITestCase, force_authenticate
+from productos.models import producto
+from productos.serializers import ProductoSerializer
 
-# Create your tests here.
+class AlmacenTestCase(APITestCase):
+    def setUp(self):
+        self.username = 'john_doe'
+        self.password = 'foobar'
+        self.user = User.objects.create(username=self.username, password=self.password)
+        self.client.force_authenticate(user=self.user)
+        producto.objects.create(
+            SKU='N.ISE32RS', descripcion='Celular')
+        producto.objects.create(
+            SKU='N.J7NEOD', descripcion='Celular')
+
+    def test_get_lista(self):
+        response = self.client.get('/api/productos/', format='json')
+        productos = producto.objects.all()
+        serializer = ProductoSerializer(productos, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, 200)
